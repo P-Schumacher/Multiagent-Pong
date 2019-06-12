b4101089d6fde4655485bad19ccbd12aca4d6678
Pierre <pier_schumacher@hotmail.com>
2019-06-12 15:07:53 +0200

    small changes

--- multiplayer_terminal_test/multiculti.py
+++ multiplayer_terminal_test/multiculti.py
@@ -6,36 +6,39 @@ import wrappers
 import gym.wrappers
 import time
 
-def play(env, net, recorder):
+RECORD = False  # do you want to record?
+
+def play(env, net, recorder, video):
     episode_n = 0
     while 1:
         episode_n += 1
         obs = env.reset()
         
         while 1:
-            #time.sleep(0.001)
-            recorder.capture_frame()
+            if not video: time.sleep(0.001)
+            if video: recorder.capture_frame()
             action = net(torch.tensor(obs, dtype=torch.float32)).max(0)[1]
             action = action.item()
             action = int(action)
-            # action = action_tuple[action]
             obs, rew, done, info = env.step(action)
             if done: break
-        recorder.close()
+        if video: recorder.close()
         break
 
 
 
 player_n = int(sys.argv[2])
-if player_n == 0:
-	video = True
-else:
-	video = False
+
 
 env = gym.make("RoboschoolPong-v1")
 env.unwrapped.multiplayer(env, game_server_guid=sys.argv[1], player_n=player_n)
-recorder = gym.wrappers.monitoring.video_recorder.VideoRecorder(env, "./recording.mp4", enabled=video)
 net = DQN(13, 4)
 net.load_state_dict(torch.load("RoboschoolPong-v1-best_night_training.dat"))
 env = wrappers.action_space_discretizer(env, 2)
-play(env, net, recorder)   # set video = player_n==0 to record video
+
+if RECORD and player_n == 0:
+    video = True
+else:
+    video = False
+recorder = gym.wrappers.monitoring.video_recorder.VideoRecorder(env, "./recording.mp4", enabled=video)
+play(env, net, recorder, video)
